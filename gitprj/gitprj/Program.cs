4751d0c50f480b9f560af1f2ff63c361f194e8b7
Begüm,ÖNDER <begumonderr38@gmail.com>
2016-06-15 14:51:53 +0300

    hazırlık

---
+++ gitprj/gitprj/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitprj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0;
+            while (sayi < 11)
+            {
+                Console.WriteLine(sayi + ".sayı");
+                sayi++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
